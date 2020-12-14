cd25b2beec60f18a4b22b8529990489e646471c3
Marinarium <schwmk@gmail.com>
2020-12-14 14:11:32 +0300

    init: covid-19 task

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import '../css/style.css';
+import '../css/style.scss';
+import {addMap} from './open-street-map.js';
+/*import {showMenu} from './showMenu';
+import {changeDeckOfCards} from './changeDeckOfCards.js';
+showMenu();
+changeDeckOfCards();*/
+
+addMap();
+
+
+
+
+
+
